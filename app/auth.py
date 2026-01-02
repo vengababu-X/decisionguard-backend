493c4039c7a4f8c26ca825bd9156fc42d5b27ea9
Boligorla vengababu <boligorlavengababu@gmail.com>
2026-01-02 22:08:26 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,11 @@
-from fastapi import Header, HTTPException
+from fastapi import Depends, HTTPException, Header
+import os
 
-API_KEYS = {"demo-key-123"}
+API_KEYS = {
+    os.getenv("COMPANY_API_KEY"): "company"
+}
 
-def verify_key(x_api_key: str = Header(...)):
+def authenticate(x_api_key: str = Header(...)):
     if x_api_key not in API_KEYS:
         raise HTTPException(status_code=401, detail="Invalid API key")
+    return API_KEYS[x_api_key]
