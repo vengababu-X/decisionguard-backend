6f7a6ad33d5a178bd2ad60ecdca1236f9e71ee59
Boligorla vengababu <boligorlavengababu@gmail.com>
2026-01-02 22:05:31 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from config import DATABASE_URL
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./decisionguard.db"
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
